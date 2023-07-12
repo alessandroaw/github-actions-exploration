dccd07768fc8723dada31e424c54da85fa65bdc9
sandro <assandroa95@gmail.com>
2023-07-12 13:33:19 +0700

    refactor redis

--- src/redis.ts
+++ src/redis.ts
@@ -1,15 +1,21 @@
 import Redis from "ioredis";
 
-console.log("hello world");
-export const redisClient = new Redis(process.env.REDIS_URL);
+const main = async () => {
+  console.log("hello world");
+  try {
+    const redisClient = new Redis(process.env.REDIS_URL);
 
-redisClient.set("mykey", "value"); // Returns a promise which resolves to "OK" when the command succeeds.
+    await redisClient.set("mykey", "value"); // Returns a promise which resolves to "OK" when the command succeeds.
 
-// ioredis supports the node.js callback style
-redisClient.get("mykey", (err, result) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(result); // Prints "value"
+    // ioredis supports the node.js callback style
+    const data = await redisClient.get("mykey");
+
+    console.log(data);
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
   }
-});
+};
+
+main();
